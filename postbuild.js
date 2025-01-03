afe404cbb440c729cdbbbff489a51cd12a4a5889
Kevin Hurley <kevinhurley23@gmail.com>
2025-01-03 10:33:02 -0500

    add gh-pages, remove postbuild script

--- postbuild.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs-extra";
-import path from "path";
-const srcDir = path.resolve("dist");
-const destDir = "C:\\xampp\\htdocs\\finance-tracker";
-fs.remove(destDir)
-  .then(() => fs.copy(srcDir, destDir))
-  .then(() => console.log("dist folder copied to C:\\xampp\\htdocs"))
-  .catch((err) => console.error(err));
